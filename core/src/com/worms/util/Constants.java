54e871a3d9b394f88aa7f9d540ecbdcce43c494f
playerony <pawel.wojtasinski.1995@gmail.com>
2017-04-06 15:35:58 +0200

    Create Constants class

---
+++ core/src/com/worms/util/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.worms.util;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class Constants {
+    public static String GAME_NAME = "Worms world party";
+    
+    public static int SCREEN_WIDTH = 700;
+    public static int SCREEN_HEIGHT = 480;
+    public static int FPS_LIMIT = 60;
+}
