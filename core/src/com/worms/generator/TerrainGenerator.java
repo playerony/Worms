a87a571073da19c8f247bc31d8563b34a322bfca
playerony <pawel.wojtasinski.1995@gmail.com>
2017-04-07 18:05:15 +0200

    Create TerrainGenerator class

---
+++ core/src/com/worms/generator/TerrainGenerator.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.worms.generator;
+
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author pawel_000
+ */
+public class TerrainGenerator {
+    private int width;
+    private int height;
+    
+    private int frequency;
+    
+    private Vector2 position;
+    
+    private ArrayList<Vector2> points;
+        
+    public TerrainGenerator(Vector2 postion, int width, int height, int frequency){
+        this.position = postion;
+        this.width = width;
+        this.height = height;
+        this.frequency = frequency;
+        
+        init();
+    }
+
+    private void init() {
+        initInstances();
+        generateTerrain();
+    }
+    
+    private void initInstances(){
+        points = new ArrayList<Vector2>();
+    }
+    
+    private void generateTerrain(){
+        generatePoints();
+        generatePointsPosition();
+    }
+    
+    private void generatePoints(){
+        for(float i=position.x ; i<=position.x + width ; i+=frequency)
+            points.add(new Vector2(i, position.y));
+    }
+    
+    private void generatePointsPosition(){
+        int i=0;
+        for(Vector2 v : points){
+            if(i%2==0)
+                v.y = MathUtils.random(position.y, height);
+                
+            i++;
+        }
+        
+        for(int j=2 ; j<20 ; j++)
+            for(i=1 ; i<points.size() - 1 ; i+=j)
+                points.get(i).y = (points.get(i - 1).y + points.get(i + 1).y) / 2;
+    }
+
+    /**
+     * 
+     * Getters and setters
+     * 
+     * @return
+     * 
+     */
+    
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+    
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public ArrayList<Vector2> getPoints() {
+        return points;
+    }
+    
+    
+}
