941aafc19b362ac63cad212b5279d773d21b6bdb
playerony <pawel.wojtasinski.1995@gmail.com>
2017-04-15 08:36:19 +0200

    Add terrain destruction

--- core/src/com/worms/controller/TerrainController.java
+++ core/src/com/worms/controller/TerrainController.java
@@ -7,7 +7,6 @@ package com.worms.controller;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.worms.generator.TerrainGenerator;
 import com.worms.object.bomb.Bomb;
@@ -24,6 +23,10 @@ public class TerrainController {
     private TerrainGenerator terrainGenerator;
     
     private ArrayList<Vector2> points;
+    private ArrayList<Vector2> pointsToRemove;
+    private ArrayList<Vector2> pointsToAdd;
+    
+    private int index = -1;
     
     public TerrainController(WorldController worldController){
         this.worldController = worldController;
@@ -33,19 +36,12 @@ public class TerrainController {
     
     private void init(){
         terrainGenerator = new TerrainGenerator(new Vector2(0f, 20f), Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT / 2, 5);
+        pointsToRemove = new ArrayList<Vector2>();
+        pointsToAdd = new ArrayList<Vector2>();
         points = terrainGenerator.getPoints();
     }
     
     public void update(){
-        for(Bomb b : worldController.getBombController().getBombs()){
-            for(int i=1 ; i<points.size() ; i++){
-                if(isCollision(new Vector2(points.get(i - 1).x, points.get(i - 1).y), new Vector2(points.get(i).x, points.get(i).y), b)){
-                    explosion(b);
-                    worldController.getBombController().getBombsToRemove().add(b);
-                }
-            }
-        }
-        
         for(int i=1 ; i<points.size() ; i++){
             float x1 = points.get(i - 1).x;
             float y1 = points.get(i - 1).y;
@@ -54,19 +50,42 @@ public class TerrainController {
             
             float distance = (float) Math.sqrt(( x2 - x1 ) *( x2 - x1 ) +( y2 - y1 ) *( y2 - y1 ) );
             
-            if(distance >= 2f){
+            if(distance >= 1f){
                 points.add(i, new Vector2((x1 + x2) / 2, (y1 + y2) / 2));
             }
         }
+        
+        for(Bomb b : worldController.getBombController().getBombs()){
+            for(int i=1 ; i<points.size() ; i++){
+                if(isCollision(new Vector2(points.get(i - 1).x, points.get(i - 1).y), new Vector2(points.get(i).x, points.get(i).y), b)){
+                    explosion(b);
+                    worldController.getBombController().getBombsToRemove().add(b);
+                    break;
+                }
+            }
+        }
+        
+        if(index != -1){
+            points.addAll(index, pointsToAdd);
+            pointsToAdd.clear();
+            
+            index = -1;
+        }
+        
+        for(Vector2 v : pointsToRemove)
+            points.remove(v);
+        
+        pointsToRemove.clear();
     }
     
     private void explosion(Bomb bomb){
-        int i=0;
+        int i = 0;
         Vector2 firstElement = new Vector2();
         Vector2 endElement = new Vector2();
         boolean isGood = false;
         boolean lastGood = false;
         
+        // Wyszukiwanie pierwszego i ostatniego punktu
         for(Vector2 v : points){
             if(i > 0){
                 float distance = (float) Math.sqrt(( v.x - bomb.getPosition().x ) *( v.x  - bomb.getPosition().x ) +( v.y - bomb.getPosition().y ) *( v.y - bomb.getPosition().y ) );
@@ -87,30 +106,47 @@ public class TerrainController {
             i++;
         }
         
-        // Liczenie nowych Y
-        
-        boolean start = false;
-        int elements = points.indexOf(endElement) - points.indexOf(firstElement);
-        
-        float startAngle = (float) ((firstElement.x / bomb.getExplosionRange()) * 180f / Math.PI);
-        float endAngle = (float) ((endElement.x / bomb.getExplosionRange()) * 180f / Math.PI);
-        
-        float angle = startAngle;
-        float angleToAdd = (endAngle - startAngle) / elements;
-        
-        for(i=0 ; i<points.size() ; i++){
-            if(points.get(i) == firstElement)
-                start = true;
-            else if(points.get(i) == endElement){
-                start = false;
-                break;
+        //Wyznaczanie nowych punktów
+        boolean isStart = false;
+        boolean isEnd = false;
+        float angle = 0f;
+        int j = 0;
+        do{
+            angle += 4f;
+            if(angle >= 360f)
+                angle = 0f;
+            
+            float x = (float) (bomb.getExplosionRange() * Math.cos(angle * Math.PI / 180f) + bomb.getPosition().x);
+            float y = (float) (bomb.getExplosionRange() * Math.sin(angle * Math.PI / 180f) + bomb.getPosition().y);
+            
+            float distance1 = (float) Math.sqrt(( firstElement.x - x ) *( firstElement.x  - x ) +( firstElement.y - y ) *( firstElement.y - y ) );
+            float distance2 = (float) Math.sqrt(( endElement.x - x ) *( endElement.x  - x ) +( endElement.y - y ) *( endElement.y - y ) );
+            
+            if(distance1 <= 1f)
+                isStart = true;
+            
+            if(isStart){
+                Vector2 po = new Vector2(x, y);
+                pointsToAdd.add(po);
+                
+                if(distance2 < 1f)
+                    isEnd = true;
             }
             
-            if(start){
-                angle += angleToAdd;
-                points.get(i).y = (float) (bomb.getExplosionRange() * -Math.sin(angle * Math.PI / 180F) + bomb.getPosition().y);
+            if(isStart && isEnd)
+                break;
+        }while(true);
+        
+        // Usuwanie punktów w kole
+        for(Vector2 v : points){
+            float distance = (float) Math.sqrt(( v.x - bomb.getPosition().x ) *( v.x  - bomb.getPosition().x ) +( v.y - bomb.getPosition().y ) *( v.y - bomb.getPosition().y ) );
+
+            if(distance < bomb.getExplosionRange()){
+                pointsToRemove.add(v);
             }
         }
+        
+        index = points.indexOf(firstElement);
     }
     
     public void render(float delta) {
