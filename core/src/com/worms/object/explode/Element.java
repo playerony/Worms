903e1eae512ab39a510732e951587c32c3cff4ad
playerony <pawel.wojtasinski.1995@gmail.com>
2017-04-16 19:22:25 +0200

    Create rotation elements

--- core/src/com/worms/object/explode/Element.java
+++ core/src/com/worms/object/explode/Element.java
@@ -6,7 +6,9 @@
 package com.worms.object.explode;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.worms.object.AbstractObject;
 import com.worms.util.Constants;
@@ -18,17 +20,28 @@ import com.worms.util.Constants;
 public class Element extends AbstractObject{
     private float xVel;
     private float yVel;
+    private float angle = 0f;
+    private float angleToAdd = 0f;
     
     public Element(Vector2 position, float size, float xVel, float yVel) {
         super(position, size, size);
         
         this.xVel = xVel;
         this.yVel = yVel;
+        
+        init();
+    }
+    
+    private void init(){
+        angleToAdd = MathUtils.random(-7.5f, 7.5f);
     }
     
     public void render(float delta){
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.circle(position.x * Constants.SCALE, position.y * Constants.SCALE, width * Constants.SCALE);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(position.x * Constants.SCALE, position.y * Constants.SCALE, 
+                                         width * Constants.SCALE / 2, height * Constants.SCALE / 2, 
+                                         width * Constants.SCALE, height * Constants.SCALE, 1f, 1f, angle);
         shapeRenderer.end();
     }
     
@@ -37,5 +50,9 @@ public class Element extends AbstractObject{
         this.position.y += yVel * Gdx.graphics.getDeltaTime();
         
         yVel += Constants.GRAVITY * Gdx.graphics.getDeltaTime();
+        angle += angleToAdd;
+        
+        if(Math.abs(angle) >= 360f)
+            angle = 0f;
     }
 }
